c2d074cc7038731a25927904f9bfd93f6a19c974
Cezar Augusto <boss@cezaraugusto.net>
2019-05-21 18:20:36 -0300

    add new noScript interface - * scripts now show intermediary state "allowed once" and "blocked once" * lists with zero items are now hidden * add ability to see grouped scripts
    address https://github.com/brave/brave-browser/issues/2996

--- components/test/brave_extension/state/shieldsPanelState_test.ts
+++ components/test/brave_extension/state/shieldsPanelState_test.ts
@@ -4,6 +4,7 @@
 
 import * as deepFreeze from 'deep-freeze-node'
 import * as shieldsPanelState from '../../../brave_extension/extension/brave_extension/state/shieldsPanelState'
+import * as noScriptState from '../../../brave_extension/extension/brave_extension/state/noScriptState'
 import { State } from '../../../brave_extension/extension/brave_extension/types/state/shieldsPannelState'
 
 const state: State = deepFreeze({
@@ -680,7 +681,7 @@ describe('shieldsPanelState test', () => {
             javascriptBlocked: 1,
             fingerprintingBlocked: 0,
             noScriptInfo: {
-              'https://test.brave.com/': { actuallyBlocked: true, willBlock: true, userInteracted: false }
+              'https://test.brave.com': { actuallyBlocked: true, willBlock: true, userInteracted: false }
             },
             adsBlockedResources: [],
             fingerprintingBlockedResources: [],
@@ -770,7 +771,7 @@ describe('shieldsPanelState test', () => {
     }
     it('reset noScriptInfo for a specific tab without navigating away', () => {
       this.tabId = 2
-      expect(shieldsPanelState.resetNoScriptInfo(
+      expect(noScriptState.resetNoScriptInfo(
         stateWithAllowedScriptOrigins, this.tabId, 'https://brave.com')).toEqual({
           currentWindowId: 1,
           tabs: {
@@ -793,8 +794,16 @@ describe('shieldsPanelState test', () => {
               fingerprintingBlocked: 0,
               url: 'https://brave.com',
               noScriptInfo: {
-                'https://a.com': { actuallyBlocked: true, willBlock: true, userInteracted: false },
-                'https://b.com': { actuallyBlocked: true, willBlock: false, userInteracted: false }
+                'https://a.com': {
+                  actuallyBlocked: true,
+                  userInteracted: false,
+                  willBlock: true
+                },
+                'https://b.com': {
+                  actuallyBlocked: true,
+                  userInteracted: false,
+                  willBlock: false
+                }
               },
               adsBlockedResources: [],
               trackersBlockedResources: [],
@@ -839,7 +848,7 @@ describe('shieldsPanelState test', () => {
     })
     it('reset noScriptInfo for a specific tab with navigating away', () => {
       this.tabId = 2
-      expect(shieldsPanelState.resetNoScriptInfo(
+      expect(noScriptState.resetNoScriptInfo(
         stateWithAllowedScriptOrigins, this.tabId, 'https://test.brave.com')).toEqual({
           currentWindowId: 1,
           tabs: {
