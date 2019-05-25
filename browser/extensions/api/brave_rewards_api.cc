edf3226a1ede54f3bfe69fc470ab50af35797bb6
Emerick Rogul <erogul@brave.com>
2019-05-24 21:06:19 -0400

    Twip from panel must use Twitter handle not name

--- browser/extensions/api/brave_rewards_api.cc
+++ browser/extensions/api/brave_rewards_api.cc
@@ -74,6 +74,8 @@ ExtensionFunction::ResponseAction BraveRewardsTipSiteFunction::Run() {
 
   auto params_dict = std::make_unique<base::DictionaryValue>();
   params_dict->SetString("publisherKey", params->publisher_key);
+  params_dict->SetString(
+      "url", contents ? contents->GetLastCommittedURL().spec() : std::string());
   ::brave_rewards::OpenTipDialog(contents, std::move(params_dict));
 
   return RespondNow(NoArguments());
@@ -143,6 +145,8 @@ void BraveRewardsTipTwitterUserFunction::OnTwitterPublisherInfoSaved(
 
   auto params_dict = std::make_unique<base::DictionaryValue>();
   params_dict->SetString("publisherKey", publisher_info->id);
+  params_dict->SetString(
+      "url", contents ? contents->GetLastCommittedURL().spec() : std::string());
 
   auto tweet_meta_data_dict = std::make_unique<base::DictionaryValue>();
   tweet_meta_data_dict->SetString("name", publisher_info->name);
